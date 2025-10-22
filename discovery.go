bfcffa2b860ce097d073d34d3c51dbead0a2efb2
995933447 <34621597+995933447@users.noreply.github.com>
2025-10-22 08:52:56 +0200

    Add files via upload

---
+++ discovery.go
@@ -0,0 +1,87 @@
+package discovery
+
+import (
+	"context"
+	"errors"
+	"math/rand"
+)
+
+var (
+	ErrSrvNotFound  = errors.New("service not found")
+	ErrNodeNotFound = errors.New("node not found")
+)
+
+const (
+	NodeStateNil   = 0
+	NodeStateAlive = 1
+	NodeStateDead  = 2
+)
+
+func NewNode(host string, port int) *Node {
+	return &Node{
+		Host: host,
+		Port: port,
+	}
+}
+
+type Node struct {
+	Host      string `json:"ip"`
+	Port      int    `json:"port"`
+	Status    int    `json:"status"`
+	Priority  int    `json:"priority"` // as high as bigger
+	Name      string `json:"name"`
+	SlaveFlag int    `json:"slave_flag"`
+	Extra     string `json:"extra"`
+}
+
+func (n *Node) Available() bool {
+	return (n.Status & NodeStateDead) == 0
+}
+
+func RouteRand(ctx context.Context, dis Discovery, srvName string) (*Node, error) {
+	srv, err := dis.Discover(ctx, srvName)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(srv.Nodes) == 0 {
+		return nil, ErrNodeNotFound
+	}
+
+	nodeNum := len(srv.Nodes)
+	randN := rand.Int()
+	for i := 0; i < nodeNum; i++ {
+		node := srv.Nodes[randN%nodeNum]
+		if node.Available() {
+			return node, nil
+		}
+		randN++
+	}
+
+	return nil, ErrNodeNotFound
+}
+
+type Service struct {
+	SrvName string  `json:"srv_name"`
+	Nodes   []*Node `json:"nodes"`
+}
+
+type Evt int
+
+const (
+	EvtNil Evt = iota
+	EvtDeleted
+	EvtUpdated
+)
+
+type OnSrvUpdatedFunc func(ctx context.Context, evt Evt, srv *Service)
+
+type Discovery interface {
+	LoadAll(ctx context.Context) ([]*Service, error)
+	Register(ctx context.Context, srvName string, node *Node) error
+	Unregister(ctx context.Context, srvName string, node *Node, remove bool) error
+	UnregisterAll(ctx context.Context, srvName string) error
+	Discover(ctx context.Context, srvName string) (*Service, error)
+	OnSrvUpdated(OnSrvUpdatedFunc)
+	Unwatch()
+}
---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/995933447/discovery
+
+go 1.24.5
+
+require (
+	github.com/fsnotify/fsnotify v1.9.0 // indirect
+	github.com/gofrs/flock v0.13.0 // indirect
+	github.com/howeyc/fsnotify v0.9.0 // indirect
+	golang.org/x/sys v0.37.0 // indirect
+)
